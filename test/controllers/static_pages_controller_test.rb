fda960bb92a5c396527fa9e7f2d06a1776366d2c
Dan Ohlin <dan.ohlin@outlook.com>
2016-12-09 20:11:04 +0000

    Add base static pages

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @base_title = "Web App"
+  end
+  
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+  
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+    assert_select "title", "#{@base_title} - Home"
+  end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "#{@base_title} - About"
+  end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "#{@base_title} - Contact"
+  end
+
+end
